0462a50e8ac85dfd3429b63545f21c0a119b9562
suyashdworkspace <suyashd.workspace@gmail.com>
2026-06-19 14:19:05 +0530

    Update vite.config.ts base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/omnipost/', // GitHub Pages base path
+  base: '/OmniPost/', // GitHub Pages base path
   build: {
     outDir: 'dist',
     sourcemap: true,
